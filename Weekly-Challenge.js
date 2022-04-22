9fcfd6e2b9ab22825437c52e74d5c1a81d86b8f1
paginesher <paginesher@gmail.com>
2022-04-22 13:58:21 -0400

    Sorry I keep blanking

---
+++ Weekly-Challenge.js
@@ -0,0 +1,18 @@
+// I am pretty sure I am overthinking all of this .
+// Everytime we do challenges I blank and freak out .
+
+const friend1 = " PJ";
+const friend2 = "Jodi";
+const friend3 = "Jordan";
+const friend4 ="Justin";
+const friend5 ="Faith";
+
+const friends = ( " PJ", "Jodi","Jordan", "Justin","Faith");
+console.log(friends);
+
+const years = new Array(1996, 1993 , 1994, 1990 ,1991);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
